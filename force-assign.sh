3f31174b0f447fef8ecda1e56533cf21a0a439a6
Alexander Alber <metalseargolid@gmail.com>
2016-07-28 12:42:20 -0400

    Make things less hard-coded

--- force-assign.sh
+++ force-assign.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# SET THESE BEFORE RUNNING OR SET ENVVARS EXTERNALLY
+[[ -z $ES_MASTER]] && ES_MASTER="IP_OR_DNS_NAME"
+[[ -z $ES_PORT ]] && ES_PORT="9200" # Port that ES_MASTER is listening on
+[[ -z $AT_A_TIME ]] && AT_A_TIME=100 # The number of unassigned shards to allocate per run
+
 set -e
 
 TARGET_NODE="$1"
@@ -11,9 +16,7 @@ fi
 
 (
 IFS=$'\n'
-for line in $(curl -XGET -s http://172.31.37.220:9200/_cat/shards | fgrep UNASSIGNED | head -n100 ); do
-    echo $line
-    
+for line in $(curl -XGET -s "http://$ES_MASTER:$ES_PORT/_cat/shards" | fgrep UNASSIGNED | head -n$AT_A_TIME ); do
     index=$(echo $line | awk '{print $1}')
     shard=$(echo $line | awk '{print $2}')
 
@@ -23,7 +26,7 @@ for line in $(curl -XGET -s http://172.31.37.220:9200/_cat/shards | fgrep UNASSI
     fi
 
     echo reallocating index $index: shard $shard to node $TARGET_NODE
-    curl -XPOST -s 'http://172.31.37.220:9200/_cluster/reroute' -d "{
+    curl -XPOST -s "http://$ES_MASTER:$ES_PORT/_cluster/reroute" -d "{
         \"commands\" : [ {
               \"allocate\" : {
                   \"index\" : \"$index\", 
